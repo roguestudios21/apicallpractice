c0c361b57f1fe044898d51c1dfb1bed2fc8249bf
roguestudios21 <atharv.choughule@gmail.com>
2025-07-10 16:50:16 +0530

    Initial Commit

---
+++ apicallpractice/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  apicallpractice
+//
+//  Created by Atharv  on 10/07/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ apicallpractice/apicallpracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  apicallpracticeApp.swift
+//  apicallpractice
+//
+//  Created by Atharv  on 10/07/25.
+//
+
+import SwiftUI
+
+@main
+struct apicallpracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
